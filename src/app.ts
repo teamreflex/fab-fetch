c0bf43d6c9846e1796b09099cea1f6086b2909fa
xKairu <kairu@team-reflex.com>
2022-02-25 05:20:37 +1300

    add twitter posting

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import 'dotenv/config'
 import { main, startup } from './main'
 
 // config fetching
-const post = process.argv.includes('--no-posting')
+const post = !process.argv.includes('--no-posting')
 const devMode = process.argv.includes('--dev')
 const timeout = Number(process.env.TIMEOUT)
 
 await startup()
 if (devMode) {
-  await main()
+  await main(post)
 } else {
-  setInterval(main, timeout)
+  setInterval(async () => main(post), timeout)
 }
