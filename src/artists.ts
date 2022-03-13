dedf6487afcb5f0908e99829589ae8eace1d0097
xKairu <kairu@team-reflex.com>
2022-03-14 01:51:22 +1300

    add profile image support

--- src/artists.ts
+++ src/artists.ts
@@ -1,7 +1,17 @@
+import { TwitterApi } from 'twitter-api-v2';
+import { formatTweet, postTweet, twitterClient } from './twitter.js';
+import { DateTime } from "luxon";
 import { getRepository } from "typeorm";
 import { getEmoji } from "./emoji.js";
 import { Artist } from "./entity/Artist.js";
+import { ProfileBanner } from "./entity/ProfileBanner.js";
+import { ProfilePicture } from "./entity/ProfilePicture.js";
 import { request } from "./http.js";
+import { FabUser, TwitterAccount } from "./types.js";
+import chalk from 'chalk';
+import { downloadImage } from './files.js';
+
+// holy shit this is so ugly
 
 export const loadArtists = async (): Promise<Artist[]> => {
   let artists = await fetchFromDatabase();
@@ -29,8 +39,8 @@ const fetchFromRemote = async (): Promise<Artist[]> => {
 
   // add the loona user first
   const artist = new Artist()
-  artist.artistId = data.group.id // this isn't correct for groups after loona
-  artist.nameEn = 'LOONA' // hardcoding this because the real one is "LOOΠΔ" :///
+  artist.artistId = data.group.id // this doesn't send the group's userId, but loona just happens to be groupId === 1 and userId === 1
+  artist.nameEn = 'LOONA' // hardcoding this because the real one is "LOOΠΔ" :/
   artist.nameKr = data.group.name
   artist.emoji = getEmoji(data.group.id)
   await getRepository(Artist).save(artist)
@@ -50,3 +60,84 @@ const fetchFromRemote = async (): Promise<Artist[]> => {
   return artists
 }
 
+const handleProfilePicture = async (user: FabUser, artist: Artist, twitter?: TwitterApi): Promise<void> => {
+  const downloadFolder = process.env.DOWNLOAD_FOLDER
+  const name = artist.nameEn
+  const folder = `${downloadFolder}/${name}/profile-pictures`
+
+  // check if profile picture url exists in the database
+  const profilePicture = await getRepository(ProfilePicture).findOne({
+    where: {
+      url: user.profileImage
+    }
+  })
+
+  if (profilePicture) {
+    return;
+  }
+
+  // if not, save it to the database
+  console.info(chalk.green(`Found new profile picture for: ${name}`))
+  const newProfilePicture = new ProfilePicture()
+  newProfilePicture.artist = artist
+  newProfilePicture.createdAt = DateTime.now().toISO()
+  newProfilePicture.url = user.profileImage
+  newProfilePicture.folder = folder
+  newProfilePicture.path = `${folder}/${user.profileImage.split('/').pop()}`
+  await getRepository(ProfilePicture).save(newProfilePicture)
+
+  // download the image
+  await downloadImage(newProfilePicture)
+
+  // send off to twitter
+  if (twitter) {
+    let text = formatTweet(DateTime.now().toISO(), artist.emoji)
+    text = `${text}\nNew profile picture!`
+    await postTweet(twitter, [newProfilePicture], text)
+  }
+}
+
+const handleProfileBanner = async (user: FabUser, artist: Artist, twitter?: TwitterApi): Promise<void> => {
+  const downloadFolder = process.env.DOWNLOAD_FOLDER
+  const name = artist.nameEn
+  const folder = `${downloadFolder}/${name}/profile-banners`
+
+  // check if profile banner url exists in the database
+  const profileBanner = await getRepository(ProfileBanner).findOne({
+    where: {
+      url: user.bannerImage
+    }
+  })
+
+  if (profileBanner) {
+    return;
+  }
+
+  // if not, save it to the database
+  console.info(chalk.green(`Found new profile banner for: ${name}`))
+  const newProfileBanner = new ProfileBanner()
+  newProfileBanner.artist = artist
+  newProfileBanner.createdAt = DateTime.now().toISO()
+  newProfileBanner.url = user.bannerImage
+  newProfileBanner.folder = folder
+  newProfileBanner.path = `${folder}/${user.profileImage.split('/').pop()}`
+  await getRepository(ProfileBanner).save(newProfileBanner)
+
+  // download the image
+  await downloadImage(newProfileBanner)
+
+  // send off to twitter
+  if (twitter) {
+    let text = formatTweet(DateTime.now().toISO(), artist.emoji)
+    text = `${text}\nNew profile banner!`
+    await postTweet(twitter, [newProfileBanner], text)
+  }
+}
+
+export const handleProfile = async (user: FabUser, artist: Artist): Promise<void> => {
+  const post = process.env.TWITTER_ENABLED === 'true'
+  const twitter = post ? twitterClient(TwitterAccount.PROFILES) : undefined
+
+  await handleProfilePicture(user, artist, twitter)
+  await handleProfileBanner(user, artist, twitter)
+}
--- src/twitter.ts
+++ src/twitter.ts
@@ -1,39 +1,47 @@
-import { getRepository } from 'typeorm';
 import chalk from 'chalk'
-import { getEmoji } from './emoji.js'
-import { DownloadPath, ParsedMessage } from './types.js'
+import { DownloadableImage, TwitterAccount } from './types.js'
 import { TweetV1, TwitterApi } from 'twitter-api-v2'
-import { Message, MessageType } from './entity/Message.js'
 import { DateTime } from 'luxon'
 
-export const twitterClient = (): TwitterApi => {
-  return new TwitterApi({
-    appKey: process.env.TWITTER_API_KEY as string,
-    appSecret: process.env.TWITTER_API_SECRET as string,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN as string,
-    accessSecret: process.env.TWITTER_ACCESS_SECRET as string,
-  })
+export const twitterClient = (account: TwitterAccount = TwitterAccount.ARCHIVE): TwitterApi => {
+  switch (account) {
+    case TwitterAccount.ARCHIVE:
+      return new TwitterApi({
+        appKey: process.env.TWITTER_ARCHIVE_API_KEY as string,
+        appSecret: process.env.TWITTER_ARCHIVE_API_SECRET as string,
+        accessToken: process.env.TWITTER_ARCHIVE_ACCESS_TOKEN as string,
+        accessSecret: process.env.TWITTER_ARCHIVE_ACCESS_SECRET as string,
+      })
+    case TwitterAccount.PROFILES:
+      return new TwitterApi({
+        appKey: process.env.TWITTER_PROFILES_API_KEY as string,
+        appSecret: process.env.TWITTER_PROFILES_API_SECRET as string,
+        accessToken: process.env.TWITTER_PROFILES_ACCESS_TOKEN as string,
+        accessSecret: process.env.TWITTER_PROFILES_ACCESS_SECRET as string,
+      })
+    default:
+      // oh fuggg
+      throw new Error(`Unknown Twitter account: ${account}`)
+  }
 }
 
-const formatTweet = (message: Message): string => {
-  const date = DateTime.fromISO(message.createdAt).toFormat('yyMMdd')
-  const time = DateTime.fromISO(message.createdAt).toFormat('hh:mma')
+export const formatTweet = (createdAt: string, emoji: string): string => {
+  const date = DateTime.fromISO(createdAt).toFormat('yyMMdd')
+  const time = DateTime.fromISO(createdAt).toFormat('hh:mma')
 
-  return `[${date}] ${message.memberEmoji}\n— ${time} KST`
+  return `[${date}] ${emoji}\n— ${time} KST`
 }
 
-export const postTweet = async (client: TwitterApi, message: Message): Promise<boolean> => {
+export const postTweet = async (client: TwitterApi, images: DownloadableImage[], text: string): Promise<boolean> => {
+  if (images.length === 0) {
+    return false
+  }
+
   console.info(chalk.cyan(`Posting to Twitter...`))
-
-  const text = formatTweet(message)
 
   // upload media to twitter
   let mediaIds = await Promise.all(
-    message.images
-      .filter(image => message.type === MessageType.POSTCARD ? image.path.includes('.mp4') : true)
-      .map(
-        image => client.v1.uploadMedia(image.path)
-      )
+    images.map(image => client.v1.uploadMedia(image.path))
   );
 
   // recursively reply to the first tweet until all images are posted
@@ -45,10 +53,6 @@ export const postTweet = async (client: TwitterApi, message: Message): Promise<b
       lastTweet = await client.v1.reply(text, lastTweet.id_str, { media_ids: mediaIds.splice(0, 4) })
     }
   }
-
-  // mark message as posted
-  message.twitterPosted = true
-  await getRepository(Message).save(message)
 
   return true
 }
